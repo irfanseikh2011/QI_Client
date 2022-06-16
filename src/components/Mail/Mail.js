9772595be3f5fc2caff6490f1315b4fb749e6356
Anika Afroz <seikhirfan2011@gmail.com>
2022-06-16 17:38:57 +0530

    added the scroll feature

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
@@ -16,13 +16,21 @@ const Mail = () => {
     console.log(user)
     const [messages] = useCollectionData(q, {idField:'id'});
 
+
+    const handleKeyPress = (event) => {
+      console.log(event.key)
+      if(event.key === 'Enter'){
+        sendMessage()
+      }
+    }
+
     // console.log(q);
 
     const avator = 'https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1655376837~hmac=bf7e3c6ce18136a3abac712fea422974';
     const [formvalue, setFormValue] = useState("");
 
     const sendMessage = async (e) => {
-        e.preventDefault();
+        // e.preventDefault();
 
         await setDoc(doc(messageRef),{
             text: formvalue,
@@ -32,8 +40,12 @@ const Mail = () => {
             displayName: user.displayName ? user.displayName :  String(user?.email).split('@')[0]
         })
 
+        setFormValue("");
+        dummy.current.scrollIntoView({behavior:'smooth'})
        
     }
+
+    const dummy = useRef()
 
 
   return (
@@ -41,14 +53,15 @@ const Mail = () => {
         <div className='mail'>
          <div className='messages'>
              {messages && messages.map(msg => <ChatMessage key ={msg.id} message = {msg} />)}
+             <div ref={dummy}></div>
         </div>
        
 
     </div>
 
     <div className="message-input">
-            <input placeholder='Enter a message..' value={formvalue} onChange={(e) => setFormValue(e.target.value)}/>
-            <button onClick={sendMessage} type="submit">Send</button>
+            <input onKeyDown={handleKeyPress} placeholder='Enter a message..' value={formvalue} onChange={(e) => setFormValue(e.target.value)}/>
+            <button  onClick={sendMessage} type="submit">Send</button>
           </div>
     </div>
     
