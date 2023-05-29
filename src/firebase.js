d3e30fda5fca0603571ef2576ae3b798c541ec09
Sekh Mohammad Irfan <seikhirfan2011@gmail.com>
2023-05-29 01:53:25 +0100

    added new firebase cred

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,26 @@ import {getAuth,GoogleAuthProvider} from "firebase/auth"
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC0JQdJTDxLoKRym1Pqa4zoxeVh3TlLoQ8",
+//   authDomain: "queue-interest.firebaseapp.com",
+//   projectId: "queue-interest",
+//   storageBucket: "queue-interest.appspot.com",
+//   messagingSenderId: "79849674144",
+//   appId: "1:79849674144:web:b12eeb5215c24e5c1ac0c5",
+//   measurementId: "G-5TJ0B7GSB5"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC0JQdJTDxLoKRym1Pqa4zoxeVh3TlLoQ8",
-  authDomain: "queue-interest.firebaseapp.com",
-  projectId: "queue-interest",
-  storageBucket: "queue-interest.appspot.com",
-  messagingSenderId: "79849674144",
-  appId: "1:79849674144:web:b12eeb5215c24e5c1ac0c5",
-  measurementId: "G-5TJ0B7GSB5"
+  apiKey: "AIzaSyDLJmEtiWuuYV9IjPVKhmI-AkLTi0RnIRk",
+  authDomain: "queueinterest12.firebaseapp.com",
+  projectId: "queueinterest12",
+  storageBucket: "queueinterest12.appspot.com",
+  messagingSenderId: "685515544097",
+  appId: "1:685515544097:web:c816571c40f1597b043560"
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
